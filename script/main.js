b54c0f6a7adc6e8329d7f9b1622519764a6dd38d
Strahinja Ninkov <ninkovs18@gmail.com>
2025-07-21 02:46:18 +0200

    poslednja izmena

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener("load", () => {
   Swal.fire({
-    title: "Pusti muziku ako želiš",
+    title: "Pusti našu pesmu",
     icon: "info",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
